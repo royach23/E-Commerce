60c7082b6f562b7a1bd3279aa16021d282afae69
Roy Achituv <roy.achituv@gmail.com>
2024-11-21 16:40:47 +0200

    feat: add delete user function

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,4 +1,5 @@
 import models.user
+from fastapi import HTTPException, status, Response
 
 def getAllUsers(db):
     all_products = db.query(models.user.User).all()
@@ -10,3 +11,13 @@ def createUser(user, db):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+def deleteUser(user_id: int, db, status_code):
+    delete_post = db.query(models.user.User).filter(models.user.User.user_id == user_id)
+    delete_post_result = delete_post.first()
+    if delete_post_result == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with such id does not exist")
+    else:
+        delete_post.delete(synchronize_session=False)
+        db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import models.user
 from app.utils.database import engine, get_db
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
 from schemas.user import Users
 import crud.user
@@ -17,14 +17,6 @@ def getAllUsers(db: Session = Depends(get_db)):
 def createUser(user: Users, db: Session = Depends (get_db)):
     return crud.user.createUser(user, db)
 
-# @app.delete("/delete/{user_id}")
-# def delete(user_id:int, db: Session = Depends(get_db), status_code = status.HTTP_204_NO_CONTENT):
-#     delete_post = db.query(models.Product).filter(models.Product.id = id)
-# if delete_post == None:
-# 6789
-# 10
-# raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail=f"product with such id does not exist")
-# else:
-# delete_post.delete(synchronize_session=False)
-# db.commit()
-# return Response(status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/user/{user_id}")
+def delete(user_id:int, db: Session = Depends(get_db), status_code = status.HTTP_204_NO_CONTENT):
+    return crud.user.deleteUser(user_id, db, status_code)
