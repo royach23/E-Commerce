6038eb0daff1392dfcb487400f3e3ef331aee692
Roy Achituv <roy.achituv@gmail.com>
2024-11-21 11:44:39 +0200

    feat(main.py): implement initial crud functionality

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 import models
-from database import engine
-from fastapi import FastAPI
+from database import engine, get_db
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from schemas import Users
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind= engine)
 
 @app.get("/users")
-def users():
+def getUsers():
     return "hello"
+
+@app.post("/users")
+def create(user: Users, db: Session = Depends (get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
---
+++ app/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class Users(BaseModel):
+    username: str
+    password: str
+    first_name: str
+    last_name: str
+    address: str
+    phone_number: str
