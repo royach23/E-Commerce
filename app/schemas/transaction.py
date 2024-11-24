2e69ff03a9edaaf5c0462565180bcd64d6748c14
Roy Achituv <roy.achituv@gmail.com>
2024-11-24 14:26:02 +0200

    fix: add proper israel timezone support

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, Field
-from datetime import datetime, timezone
+from datetime import datetime
+import pytz
 
 class Transactions(BaseModel):
     user_id: int
     total_price: float
-    purchase_time: str = Field(default_factory=datetime.now(timezone.utc).isoformat)
+    purchase_time: str = Field(default_factory=datetime.now(pytz.timezone('Israel')).isoformat)
