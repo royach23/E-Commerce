508b8bb34f440f7352746a80028bba9d90906e40
Roy Achituv <roy.achituv@gmail.com>
2024-11-24 21:57:23 +0200

    fix: minor change

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ecommerce"
-
+load_dotenv() 
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 engine = create_engine (SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker (autocommit=False, autoflush=False, bind=engine)
--- app/utils/security.py
+++ app/utils/security.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import os
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-load_dotenv()  # Load environment variables from .env file
+load_dotenv() 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
