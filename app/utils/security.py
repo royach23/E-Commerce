9e96c53bf7a42e1b2c63f9be69c6453f0d9b0f74
Roy Achituv <roy.achituv@gmail.com>
2024-11-25 09:33:43 +0200

    fix: fix login error

--- app/utils/security.py
+++ app/utils/security.py
@@ -9,7 +9,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 load_dotenv() 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -19,7 +19,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
-    expire = datetime.now(pytz.timezone('Israel')).isoformat + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = (datetime.now(pytz.timezone('Israel')) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)).isoformat()
     to_encode.update({"exp": expire})  
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
