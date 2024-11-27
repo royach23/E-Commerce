665bc75b15b331e01d4657f919140e01b9f9dcda
Roy Achituv <roy.achituv@gmail.com>
2024-11-27 17:07:38 +0200

    ref: add size field to the transaction_product table

--- app/models/transactionProduct.py
+++ app/models/transactionProduct.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Integer, Column, ForeignKey
+from sqlalchemy import Integer, Column, ForeignKey, Enum
 from app.utils.database import Base
 from sqlalchemy.orm import relationship
+from ..enums.size import Size
 
 class TransactionProduct (Base):
     __tablename__ = 'transaction_products'
     transaction_id = Column(Integer, ForeignKey("transactions.transaction_id"), primary_key=True, nullable=False)
     product_id = Column(Integer, ForeignKey("products.product_id"), primary_key=True, nullable=False)
-    quantity = Column(Integer)
+    quantity = Column(Integer, nullable=False)
+    Size = Column(Enum(Size), nullable=False)
     product = relationship("Product", backref="transaction_products")
--- app/schemas/transactionProduct.py
+++ app/schemas/transactionProduct.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+from ..enums.size import Size
 
 class TransactionProducts(BaseModel):
     transaction_id: int
     product_id: int
     quantity: int
+    size: Size
